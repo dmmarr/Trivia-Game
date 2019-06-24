5bb305c99709176e36dd6accb78e7974e94a5f2c
Damaris Magdaleno <damaris@Damariss-MacBook-Pro-2.local>
2019-06-24 00:04:43 -0700

    Homework done

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+
+var correct = 0;
+var incorrect = 0;
+var questions = [
+    {
+        question: "What PowerPuff Girl is pink ?",
+        arr: ["Blossom", "Buttercup", "Bubbles"],
+        values: [true, false, false],
+    },
+    {
+        question: "What PowerPuff Girl is green ?",
+        arr: ["Blossom", "Buttercup", "Bubbles"],
+        values: [false, true, false],
+    },
+    {
+        question: "What PowerPuff Girl is blue ?",
+        arr: ["Blossom", "Buttercup", "Bubbles"],
+        values: [false, false, true],
+    }
+]
+if {
+    
+}
