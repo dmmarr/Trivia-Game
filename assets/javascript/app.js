69c2ff7e42c5b16c1c85ec23ed41a19ad54537c6
Damaris Magdaleno <damaris@Damariss-MacBook-Pro-2.local>
2019-06-30 20:32:49 -0700

    Updated my javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,61 @@
 
 var correct = 0;
 var incorrect = 0;
+var timer ;
+var counter = 5;
 var questions = [
+    
     {
         question: "What PowerPuff Girl is pink ?",
         arr: ["Blossom", "Buttercup", "Bubbles"],
-        values: [true, false, false],
+       correctAns :"Blossom"
     },
     {
         question: "What PowerPuff Girl is green ?",
         arr: ["Blossom", "Buttercup", "Bubbles"],
-        values: [false, true, false],
-    },
+        correctAns :"Buttercup"
+    }, 
     {
         question: "What PowerPuff Girl is blue ?",
         arr: ["Blossom", "Buttercup", "Bubbles"],
-        values: [false, false, true],
+        correctAns : "Bubbles"
+    } 
+    ]
+
+    var showResults=function(){
+        clearInterval(timer)
+        alert (
+            "Time is over"
+        )
+     //show results
     }
-]
-if {
-    
+
+
+$("#start").on("click", function(e){
+$("#start").remove()
+timer=setInterval(function(){
+    counter --;
+    if (counter===0){
+ 
+ showResults();
+ 
+
+    }
+    $(".timer").text(counter)
+
+
+},1000 )
+
+
+for( var i =0; i< questions.length; i++){
+    $("#quiz").append("<h2> "+questions[i].question +"</h2>")
+    for ( var t = 0; t< questions[i].arr.length; t++){
+        $("#quiz").append("<input type='radio' name= question"+i+ "value="+ questions[i].arr[t] +">"+ questions[i].arr[t] )
+
+    }
 }
+$("#quiz").append("<button class='submit'> Submit </button>")
+ })
+$(document).on("click", ".submit", function(e){
+
+})
